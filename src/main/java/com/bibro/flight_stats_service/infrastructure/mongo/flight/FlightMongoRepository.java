793c0c362e10167ac1cb1f7b9e0d4a5221f869bb
Mikolaj Bibro <mikolaj.bibro@crustlab.com>
2022-04-10 19:16:02 +0200

    removed unused import

--- src/main/java/com/bibro/flight_stats_service/infrastructure/mongo/flight/FlightMongoRepository.java
+++ src/main/java/com/bibro/flight_stats_service/infrastructure/mongo/flight/FlightMongoRepository.java
@@ -12,7 +12,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 
 @Repository
 public class FlightMongoRepository implements FlightRepository {
